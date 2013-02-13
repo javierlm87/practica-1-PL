6c8d9c9fa49c39b2563d959274ff4297043b90a3
javi <javier_lm87@hotmail.com>
2013-02-13 10:58:05 +0000

    commit final

--- conversortemperature.js
+++ conversortemperature.js
@@ -3,25 +3,27 @@ function calcular() {
   var resultado;
   var original = document.getElementById("original");
   var temporal = original.value;
-  var regexp = /^(-|+)?\d+(.\d+)?º(f|F|C|c)$/g;
+  var regexp = /^-?\d+(.\d+)?(f|F|C|c)$/g;
   
-  var m = temp.match(regexp);
+  var m = temporal.match(regexp);
   
   if (m) {
-    var num = m[0].match(/^(-|+)?\d+(.\d+)?/);
+    var num = m[0].match(/^-?\d+(.\d+)?/);
     var type = m[0].match(/f|F|C|c/);
     num = parseFloat(num);
     if (type == 'c' || type == 'C') {
-      result = (num * 9/5)+32;
-      result = result + 'F';
+      resultado = (num * 9/5)+32;
+      resultado = resultado.toFixed(2);
+      resultado = resultado + 'F';
     }
     else {
-      result = (num - 32)*5/9;
-      result = result + 'C';
+      resultado = (num - 32)*5/9;
+      resultado = resultado.toFixed(2);
+      resultado = resultado + 'C';
     }
-    converted.innerHTML = result;
+    converted.innerHTML = resultado;
   }
   else {
-    converted.innerHTML = "ERROR al introducir la temperatura. Temperatura no válida";
+    converted.innerHTML = "ERROR al introducir la temperatura. Temperatura no valida";
   }
 }
